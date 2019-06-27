59ade59d86b39703db3b9f7d7581185530c9941a
Aaron3S <34506289+Aaron3S@users.noreply.github.com>
2019-06-27 12:40:54 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 for i in $(cat images.txt);
 do 
   docker pull $i;
-  docker tag $i localhost:8082/$i;
-  docker push localhost:8082/$i; 
+  docker tag $i localhost:8091/$i;
+  docker push localhost:8091/$i; 
 done
